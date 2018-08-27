bd35ef23697a69d3def3716b844c31d0678e73e5
mm909 <Bobar312@gmail.com>
2018-08-26 18:12:39 -0700

    Finished InsertionSort

--- Sorting/4. insertion Sort/insertion.js
+++ Sorting/4. insertion Sort/insertion.js
@@ -16,7 +16,9 @@ function setup() {
   data.setDataMax(dataMax);
   data.calcDataWidth();
   data.getNewData();
+  insertionSort(data.data);
   data.draw();
+
 }
 
 function draw(){
@@ -26,11 +28,34 @@ function draw(){
 
 /* -- Insertion sort function below  -- */
 
+/*
 
+function insertionSort(array A)
+    for i from 1 to length[A]-1 do
+        value := A[i]
+        j := i-1
+        while j >= 0 and A[j] > value do
+            A[j+1] := A[j]
+            j := j-1
+        done
+        A[j+1] = value
+    done
 
-function gnomeSort(array){
+*/
+
+function insertionSort(array){
   console.log("-- Sorting -- ")
 
+  var j = 0;
+  for(var i = 1; i < array.length; i++){
+    var value = array[i];
+    j = i - 1;
+    while(j >= 0 && array[j] > value){
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = value;
+  }
 
   console.log("-- Fnished -- ")
 }
