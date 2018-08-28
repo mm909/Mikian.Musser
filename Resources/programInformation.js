bee45b6cf42e45bcc70422077236f16613c3d030
mm909 <Bobar312@gmail.com>
2018-08-28 08:55:22 -0700

    Added in example row
    Added tags, des, title, picture.

--- Resources/programInformation.js
+++ Resources/programInformation.js
@@ -3,24 +3,24 @@ var programs = [
     number: 1,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Bubble Sort Visualization',
-    shortDescription: 'This program is a virtual representation of how the Bubble sort algorithm works.'
+    shortDescription: 'This program is a visual representation of how the Bubble sort algorithm works.'
   },
   {
     number: 2,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Count Sort Visualization',
-    shortDescription: 'This program is a virtual representation of how the Count sort algorithm works.'
+    shortDescription: 'This program is a visual representation of how the Count sort algorithm works.'
   },
   {
     number: 3,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Gnome Sort Visualization',
-    shortDescription: 'This program is a virtual representation of how the Gnome sort algorithm works.'
+    shortDescription: 'This program is a visual representation of how the Gnome sort algorithm works.'
   },
   {
     number: 4,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Insertion Sort Visualization',
-    shortDescription: 'This program is a virtual representation of how the Insertion sort algorithm works.'
+    shortDescription: 'This program is a visual representation of how the Insertion sort algorithm works.'
   }
 ]
