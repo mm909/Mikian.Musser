aa98e7f41feef973ee52bf52d8a95c002f435e9c
mm909 <Bobar312@gmail.com>
2018-08-28 22:33:56 -0700

    added download and links

--- Resources/programInformation.js
+++ Resources/programInformation.js
@@ -4,27 +4,34 @@ var programs = [
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Bubble Sort Visualization',
     shortDescription: 'Visual representation of the Bubble sort algorithm.',
-    filepath: 'Sorting/1.%20Bubble%20Sort/index.html'
+    filepath: 'Sorting/1.%20Bubble%20Sort/index.html',
+    rosettaLink: 'http://www.rosettacode.org/wiki/Sorting_algorithms/Bubble_sort',
+    description: 'This is a visual representation of the Bubble sort algorithm. \n ' +
+                 'Created with P5.js.',
+    downloadLink: 'bubbleSort.zip'
   },
   {
     number: 2,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Count Sort Visualization',
     shortDescription: 'Visual representation of the Count sort algorithm.',
-    filepath: 'Sorting/2.%20Count%20Sort/index.html'
+    filepath: 'Sorting/2.%20Count%20Sort/index.html',
+    rosettaLink: 'http://www.rosettacode.org/wiki/Sorting_algorithms/Counting_sort'
   },
   {
     number: 3,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Gnome Sort Visualization',
     shortDescription: 'Visual representation of the Gnome sort algorithm.',
-    filepath: 'Sorting/3.%20Gnome%20Sort/index.html'
+    filepath: 'Sorting/3.%20Gnome%20Sort/index.html',
+    rosettaLink: 'http://www.rosettacode.org/wiki/Sorting_algorithms/Gnome_sort'
   },
   {
     number: 4,
     tags: ['Sorting', 'JavaScript', 'Visualization'],
     title: 'Insertion Sort Visualization',
     shortDescription: 'Visual representation of the Insertion sort algorithm.',
-    filepath: 'Sorting/4.%20Insertion%20Sort/index.html'
+    filepath: 'Sorting/4.%20Insertion%20Sort/index.html',
+    rosettaLink: 'http://www.rosettacode.org/wiki/Sorting_algorithms/Insertion_sort'
   }
 ]
