29c8a5cd77a0d26a08451cf6d4af022eba833a84
mm909 <Bobar312@gmail.com>
2018-09-03 02:18:16 -0700

    Added fractal trees rec.

---
+++ CodingChallenges/17.FractalTrees/FT.js
@@ -0,0 +1,45 @@
+var a = Math.PI / 4;
+var slider;
+var length = 175
+var c = 0;
+var mainStep = 0;
+var step = 1;
+
+var r;
+var g;
+var b;
+
+function setup() {
+  var canvas = createCanvas(600,500)
+  canvas.parent("canvasContainer");
+  // slider = createSlider(0,TWO_PI, PI/4, 0.01)
+  r = random(255);
+  g = random(255);
+  b = random(255);
+}
+
+function draw() {
+  background("#C5C6Ca");
+  stroke(255)
+  strokeWeight(1)
+  // a = slider.value();
+  a+=0.01
+  translate(width/2,height)
+  branch(length,1);
+}
+
+function branch(l,level){
+  var c = map(level, 1, 10, 0, 1)
+  strokeWeight(10-level)
+  stroke(c * r, c * g, c * b)
+  line(0, 0, 0, -l)
+  translate(0,  -l)
+  if(l>10){
+    push()
+    rotate(a);
+    branch( l * .66,level+1)
+    pop();
+    rotate(-a);
+    branch( l * .66,level+1)
+  }
+}
