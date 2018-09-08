b5abd8cf3a8961b81d8a255348002e3c6345a5d6
mm909 <Bobar312@gmail.com>
2018-09-07 20:04:16 -0700

    Worked on A*

--- CodingChallenges/0.Dev/Code/sketch.js
+++ CodingChallenges/0.Dev/Code/sketch.js
@@ -2,61 +2,136 @@
 // 0: normal
 // 1: OS
 // 2: CS
-// 3: Start
+// 3: Path
 // 4: End
-var cols = 5;
-var rows = 5;
+// 5: Start
+var cols = 25;
+var rows = 25;
 var grid = [];
 var w, h;
+var path = [];
 
 var closedSet = [];
 var openSet = [];
 var Start;
 var End;
 
+var wallChance = 0.25;
+
 function setup() {
   createCanvas(500, 500);
   console.log("A*");
   w = width / cols;
   h = height / rows;
+
   for (var i = 0; i < rows; i++) {
     grid[i] = new Array(cols)
     for (var j = 0; j < cols; j++) {
       grid[i][j] = new Cell(i, j);
-      grid[i][j].g = Infinity;
-      grid[i][j].f = Infinity;
     }
   }
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
+  }
+
   Start = grid[0][0];
+  Start.wall = false;
   End = grid[rows - 1][cols - 1];
-  Start.g = 0;
-  Start.f = 0;
-  Start.h = EH(Start, End)
-  Start.c = 1;
+  End.wall = false;
   openSet.push(Start);
 }
 
 function draw() {
   background(151)
 
-  // current := the node in openSet having the lowest fScore[] value
-  var current grid[0][0];
-  var record = Infinity;
-  for (var i = 0; i < openSet.length; i++) {
-    if (current.f > openSet[i].f) {
-      current = openSet[i]
+  if(openSet.length>0){
+    var li = 0;
+    for (var i = 0; i < openSet.length; i++) {
+      if(openSet[i].f < openSet[li].f) {li = i}
     }
-  }
+    var current = openSet[li]
 
-  if (current == End) {
-    // End
+    path = [];
+    var temp = current;
+    path.push(temp)
+    while(temp.prev){
+      temp.c = 3;
+      path.push(temp.prev)
+      temp = temp.prev;
+    }
+
+    if(current === End){
+      console.log("done");
+      noLoop();
+    }
+
+    removeFromArray(openSet,current)
+    closedSet.push(current);
+    current.c = 2;
+
+    var neighbors = current.neighbors;
+    for (var i = 0; i < neighbors.length; i++) {
+      var neighbor = neighbors[i];
+      if(!closedSet.includes(neighbor) && !neighbor.wall){
+        var tempG = current.g + 1;
+        if(openSet.includes(neighbor)){
+          if(tempG < neighbor.g){
+            neighbor.g = tempG;
+          }
+        } else {
+          neighbor.g = tempG;
+          openSet.push(neighbor)
+          neighbor.c = 1;
+        }
+        neighbor.h = TaxiH(neighbor,End)
+        neighbor.f = neighbor.g + neighbor.h;
+        neighbor.prev = current;
+      }
+    }
+
+  } else {
+    // no sol
   }
 
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
+      fill(255)
+      noStroke();
       grid[i][j].show();
     }
   }
+
+  for (var i = 0; i < openSet.length; i++) {
+    fill(100,255,100)
+    noStroke();
+    var c = openSet[i];
+    rect(c.i * w, c.j * h, w - 1, h - 1);
+  }
+
+  for (var i = 0; i < closedSet.length; i++) {
+    fill(255,100,100)
+    noStroke();
+    var c = closedSet[i];
+    rect(c.i * w, c.j * h, w - 1, h - 1);
+  }
+
+  for (var i = 0; i < path.length; i++) {
+    fill(100,100,255)
+    noStroke();
+    var c = path[i];
+    rect(c.i * w, c.j * h, w - 1, h - 1);
+  }
+
+  fill(255,255,0)
+  noStroke();
+  rect(Start.i * w, Start.j * h, w - 1, h - 1);
+  fill(255,0,255)
+  noStroke();
+  rect(End.i * w, End.j * h, w - 1, h - 1);
+
 }
 
 function Cell(i, j) {
@@ -66,17 +141,97 @@ function Cell(i, j) {
   this.h = 0;
   this.f = 0;
   this.c = 0;
-  this.cameFrom = [];
+  this.prev = null;
+  this.neighbors = [];
+  this.wall = false;
+
+  if(random(1) < wallChance){
+    this.wall = true;
+  }
+
   this.show = function() {
-    if (this.c == 0) {
-      fill(255)
-    } else if (this.c == 1) {
-      fill(100, 255, 100)
+    noStroke();
+    if(this.wall){
+      fill(0)
     }
-    rect(this.i * w, this.j * h, w - 1, h - 1);
+    rect(this.i * w, this.j * h, w, h);
+  }
+
+  this.addNeighbors = function(grid){
+    var i = this.i;
+    var j = this.j;
+      if(i<rows-1)
+      this.neighbors.push(grid[i+1][j])
+      if(i>0)
+      this.neighbors.push(grid[i-1][j])
+      if(j<cols-1)
+      this.neighbors.push(grid[i][j+1])
+      if(j>0)
+      this.neighbors.push(grid[i][j-1])
+      if(i > 0 && j > 0)
+      this.neighbors.push(grid[i-1][j-1])
+      if(i > 0 && j < cols - 1)
+      this.neighbors.push(grid[i-1][j+1])
+      if(i < rows-1  && j > 0)
+      this.neighbors.push(grid[i+1][j-1])
+      if(i < rows-1 && j < cols - 1)
+      this.neighbors.push(grid[i+1][j+1])
+
+      // if(i > 0 && j > 0){
+      //   if( i < 0 && i < rows - 1 && j > 0 && j < cols - 1){
+      //     if(!grid[i+1][j].wall && !grid[i][j-1].wall){
+      //       this.neighbors.push(grid[i-1][j-1]) // TOP LEFT
+      //     }
+      //   } else {
+      //     this.neighbors.push(grid[i-1][j-1])
+      //   }
+      // }
+      //
+      // if(i > 0 && j < cols - 1){
+      //   if( i < 0 && i < rows - 1 && j > 0 && j < cols - 1){
+      //     if(!grid[i+1][j].wall && !grid[i][j+1].wall){
+      //       this.neighbors.push(grid[i-1][j+1]) // TOP RIGHT
+      //     }
+      //   } else {
+      //     this.neighbors.push(grid[i-1][j+1])
+      //   }
+      // }
+      //
+      // if(i < rows-1  && j > 0){
+      //   if( i < 0 && i < rows - 1 && j > 0 && j < cols - 1){
+      //     if(!grid[i-1][j].wall && !grid[i][j-1].wall){
+      //       this.neighbors.push(grid[i+1][j-1]) // BOTTOM LEFT
+      //     }
+      //   } else {
+      //     this.neighbors.push(grid[i+1][j-1]) // BOTTOM LEFT
+      //   }
+      // }
+      //
+      // if(i < rows-1  && j > 0){
+      //   if( i < 0 && i < rows - 1 && j > 0 && j < cols - 1){
+      //     if(!grid[i-1][j].wall && !grid[i][j+1].wall){
+      //       this.neighbors.push(grid[i+1][j+1]) // BOTTOM RIGHT
+      //     }
+      //   } else {
+      //     this.neighbors.push(grid[i+1][j+1]) // BOTTOM RIGHT
+      //   }
+      // }
+
   }
 }
 
 function EH(start, goal) {
   return dist(start.i, start.j, goal.i, goal.j)
+}
+
+function TaxiH(start, goal) {
+  return abs(start.i - goal.i) + abs(start.j - goal.j)
+}
+
+function removeFromArray(a,ele){
+  for (var i = a.length-1; i >= 0; i--) {
+    if(a[i] == ele){
+      a.splice(i,1)
+    }
+  }
 }
