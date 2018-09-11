05c69e7f5b66dadffc8dd4b3985af4bbf3e14f1e
mm909 <Bobar312@gmail.com>
2018-09-10 21:55:24 -0700

    Added buttons to each ele

---
+++ DEV/jQuery/button.js
@@ -0,0 +1,11 @@
+
+
+$( document ).ready(function() {
+  $( ".gallery-cell" ).hover(function() {
+    console.log($(this).attr("id"));
+    $i = $(this).attr("id")
+    $("#" + $i).find(".gobtn").css("visibility","visible")
+  }, function() {
+    $("#" + $i).find(".gobtn").css("visibility","hidden")
+  });
+});
