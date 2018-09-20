1761b2cc6a6d5f1181146b98d0741b574b6e14a0
mm909 <Bobar312@gmail.com>
2018-09-19 20:57:56 -0700

    Finished updating comments

--- Sprints/AdvancedProgramingTopicsWithP5/Fireworks/code/firework.js
+++ Sprints/AdvancedProgramingTopicsWithP5/Fireworks/code/firework.js
@@ -1,67 +1,144 @@
-function Firework(x, y, s, c) {
-  this.pos = createVector(x, y)
-  this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0);
-  this.g = createVector(0, 0.1);
-  this.s = s;
-  this.pCount = s * 10;
-  this.c = c;
-  this.particles = [];
-  this.exploded = false;
-  this.del = false;
+// Mikian Musser
+// Code Central
+// Sprint: Advanced Programing Topics with P5.js
+// https://www.youtube.com/watch?v=CKeyIbT3vXI
 
-  this.applyForce = function(f) {
-    this.acc.add(f);
+// 1. This is a firework object
+  // It takes in -
+  // x
+  // y
+  // size
+  // color
+
+// Vars
+// 2. vector pos
+// 3. size
+// 4. color
+// 5. vector vel
+// 6. vector acc
+// 7. vector g
+// 8. pcount
+// 9. array partical
+// 10. bool exploded
+// 11. bool del
+
+// Functions
+// 12. Apply force
+// 13. Update
+// 14. explode
+// 15. show
+
+function Firework(x_, y_, size_, color_) { // 1
+
+  // Vars
+  // 2. Create a vector with x and y as the passed values.
+  // 3. set size to the passed value
+  // 4. set the color to the passed value
+  this.pos = createVector(x_, y_)  // 2
+  this.size = size_;  // 3
+  this.color = color_;  // 4
+
+  // 5. create a vector for the velocity
+  // 6. create a vector for the acceleration
+  // 7. create a vector for the force of gravity
+  this.vel = createVector(0, 0);  // 5
+  this.acc = createVector(0, 0);  // 6
+  this.g = createVector(0, 0.1);  // 7
+
+  // 8. pCount is the number of particles the fire makes when it explodes
+  // 9. particles is the array for storing all of the particles made when it explodes
+  this.pCount = floor(size_ * 5);  // 8
+  this.particles = [];  // 9
+
+  // 10. exploded is a bool to check is the firework has been exploded yet. Starts as false
+  // 11. del is a bool to see if the firework can be deleted from the array.
+  this.exploded = false;  // 10
+  this.del = false;  // 11
+
+// 12. Apply force
+  // Apply force takes in a vector and adds it to the acceleration
+  // a. Add the force to acc vector
+  this.applyForce = function(f) { // 12
+    this.acc.add(f); // 12.a
   }
 
-  this.update = function() {
-    this.vel.add(this.acc);
-    this.vel.add(this.g)
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-    this.pos.x = constrain(this.pos.x, 0, width - this.s);
-    this.pos.y = constrain(this.pos.y, 0, height - this.s);
+// 13. Update
+  // a > Add acc to vel
+  // b > Add g to vel
+  // c > Add pos to vel
+  // d > 0 out acc
+  // e > If the vel is negitive (Firework is falling)
+    // f > then explode the firework
+  // g > If the firework has exploded and there are no particles in the air
+    // h > Mark the firework for deletion
+  this.update = function() { // 13
+    this.vel.add(this.acc); // 13.a
+    this.vel.add(this.g)    // 13.b
+    this.pos.add(this.vel); // 13.c
+    this.acc.mult(0);       // 13.d
 
-    if (this.vel.y > 0) {
-      this.explode();
+    if (this.vel.y > 0) {   // 13.e
+      this.explode();       // 13.f
     }
-    if (this.exploded && this.particles.length == 0) {
-      this.del = true;
+
+    if (this.exploded && this.particles.length == 0) { // 13.g
+      this.del = true;  // 13.h
     }
   }
 
-  this.show = function() {
-    colorMode(HSB);
-    if (!this.exploded) {
-      noStroke();
-      fill(this.c, 255, 255);
-      ellipse(this.pos.x, this.pos.y, this.s, this.s);
+// 14. explode
+  // a > if this firework has not exploded yet (We dont want to do this more than once)
+    // b > do the following pCount times
+    // c > Create a force that will send the partical flying out
+    // d > create a particle
+      // > this.pos so that the particles can start where the firework ended.
+      // > this.size/2 is the new size of the partical
+      // > the new random force we made
+      // > this.color so that we know what color to draw the particals
+    // e > apply that random for the particle
+    // f > add that partical to the array list
+  // g > set exploded to true (so that we dont do all of that again)
+  this.explode = function() { // 14
+    if (!this.exploded) { // a
+      for (var i = 0; i < this.pCount; i++) { // b
+        var force = createVector(random(-2, 2), random(-2, 2)); // c
+        var particle = new Particle(this.pos, this.size / 2, this.color); // d
+        particle.applyForce(force); // e
+        this.particles[i] = particle; // f
+      }
     }
-    for (var i = this.particles.length - 1; i >= 0; i--) {
+    this.exploded = true; // g
+  }
+
+// 15. show
+  // > this function will draw the firework and it's particles
+  // a > set the color mode to HSB (Hue, saturation, brightness)
+    // > this makes our colors really saturated and bright
+  // b > check to see if the firework has been exploded
+    // > if the fire work has exploded then dont show it
+  // c > noStroke removes black lines around our firework
+  // d > fill our circle with the color
+  // e > draw the firework at x,y,size,size
+  // f > For every partical (Loop through this array backwards)
+    // > Update the partical's location
+    // > show the partical
+    // > if the partical is marked for deletion
+      // > Delete that partical
+  this.show = function() { // 15
+    colorMode(HSB); // a
+
+    if (!this.exploded) { // b
+      noStroke(); // c
+      fill(this.color, 255, 255); // d
+      ellipse(this.pos.x, this.pos.y, this.size, this.size); // e
+    }
+
+    for (var i = this.particles.length - 1; i >= 0; i--) { // f
       this.particles[i].update();
       this.particles[i].show();
       if (this.particles[i].del) {
         this.particles.splice(i, 1)
       }
     }
-  }
-
-  this.bounce = function() {
-    var force = createVector(0, random(-9, -5))
-    if (this.pos.y == height - this.s) {
-      this.applyForce(force)
-    }
-  }
-
-  this.explode = function() {
-    if (!this.exploded) {
-      for (var i = 0; i < this.pCount; i++) {
-        var f = createVector(random(-2, 2), random(-2, 2));
-        var particle = new Particle(this, this.pos.x, this.pos.y, this.s / 2.5, f, this.c);
-        this.particles[i] = particle;
-        this.particles[i].applyForce();
-      }
-    }
-    this.exploded = true;
   }
 }
--- Sprints/AdvancedProgramingTopicsWithP5/Fireworks/code/particle.js
+++ Sprints/AdvancedProgramingTopicsWithP5/Fireworks/code/particle.js
@@ -1,20 +1,67 @@
-function Particle(f, x, y, s, force, c) {
-  this.firework = f;
-  this.pos = createVector(x, y);
+// Mikian Musser
+// Code Central
+// Sprint: Advanced Programing Topics with P5.js
+// https://www.youtube.com/watch?v=CKeyIbT3vXI
+
+// 1. this is a partical object
+  // > it takes in a parent firework
+  // > posistion
+  // > size
+  // > color
+
+// Vars
+// 2. vector pos
+// 3. size
+// 4. color
+// 5. vector vel
+// 6. vector acc
+// 7. vector g
+// 8. bool del
+// 9. life
+// 10. lifeSpan
+
+// Functions
+// 11. Apply force
+// 12. Update
+// 13. show
+
+function Particle(pos_, size_, color_) { // 1
+
+  // Vars
+  // 2. Create a vector with x and y as the passed values.
+  // 3. set size to the passed value
+  // 4. set the color to the passed value
+  this.pos = createVector(pos_.x,pos_.y);
+  this.size = size_;
+  this.color = color_;
+
+  // 5. create a vector for the velocity
+  // 6. create a vector for the acceleration
+  // 7. create a vector for the force of gravity
   this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
   this.g = createVector(0, 0.01);
-  this.s = s;
-  this.force = force;
-  this.del = false;
-  this.life = 0;
-  this.lifeSpan = 35;
-  this.c = c;
 
-  this.applyForce = function() {
-    this.acc.add(this.force);
+  // 8. del is a bool to see if the partical can be deleted from the array.
+  this.del = false;
+
+  // 9. this.life is how many frames this partical has been alive
+  // 10. this.lifeSpan is how many frames this partical is allowed to live
+  this.life = 0;
+  this.lifeSpan = 40;
+
+  // 11. Apply force
+    // Apply force takes in a vector and adds it to the acceleration
+    // a. Add the force to acc vector
+  this.applyForce = function(f) {
+    this.acc.add(f);
   }
 
+  // 12. Update
+    // a > Add acc to vel
+    // b > Add g to vel
+    // c > Add pos to vel
+    // d > 0 out acc
   this.update = function() {
     this.vel.add(this.acc);
     this.vel.add(this.g)
@@ -26,11 +73,16 @@ function Particle(f, x, y, s, force, c) {
     }
   }
 
-  this.show = function() {
-    colorMode(HSB)
-    var a = map(this.life, 0, this.lifeSpan, 255, 25)
-    fill(this.c, a, a)
-    ellipse(this.pos.x, this.pos.y, this.s, this.s)
+  // 13. Show
+    // > this function will draw the partical
+    // a > set the color mode to HSB (Hue, saturation, brightness)
+    // b > get the alpha value using the map function (Make it look like they are fading)
+    // c > fill with color and alpha
+    // d > draw the partical
+  this.show = function() { // 13
+    colorMode(HSB) // a
+    var alpha = map(this.life, 0, this.lifeSpan, 255, 50) // b
+    fill(this.color, alpha, alpha) // c
+    ellipse(this.pos.x, this.pos.y, this.size, this.size) // d
   }
-
 }
