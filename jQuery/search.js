53d40551892e6e2dae9e8a18ac5f4bb0339cfa6e
Mikian Musser <Bobar312@gmail.com>
2018-10-27 10:06:23 -0700

    Added Search functionality
    The Search will search the projects tab

--- jQuery/search.js
+++ jQuery/search.js
@@ -17,3 +17,21 @@ function filterProjects() {
     }
   }
 }
+
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+
+$(document).ready(function() {
+  let list = getUrlVars();
+  console.log(list);
+  if (list.search.length > 0) {
+
+    document.getElementById("searchBox").value = list.search;
+  }
+  filterProjects();
+});
