c6d3872ed2481014512c6c802d8bf49cccdb824f
Mikian Musser <Bobar312@gmail.com>
2018-11-25 14:58:41 -0800

    Added Base text

--- js/iam.js
+++ js/iam.js
@@ -5,17 +5,23 @@ if (debug) {
 function iam() {
   $("#iamRow").html("")
   let iam = `
-  <div id='iamrow1' class='col-sm'>
-    <p> Test </p>
-  </div>
-  <div id='iamrow2' class='col-sm'>
-    <p> Test </p>
-  </div>
-  <div id='iamrow3' class='col-sm'>
-    <p> Test </p>
-  </div>
-  <div id='iamrow4' class='col-sm'>
-    <p> Test </p>
+  <div class="iamContainer">
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <p> Test1 </p>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <p> Test2 </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <p> Test3 </p>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <p> Test4 </p>
+      </div>
+    </div>
   </div>
   `
   $tempProjectCard = $(iam)
