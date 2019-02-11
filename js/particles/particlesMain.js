8b425626c390ada3470bdc653e41b23e9d0f9879
Mikian Musser <Bobar312@gmail.com>
2019-02-11 15:10:30 -0800

    Started adding Waka time

--- js/particles/particlesMain.js
+++ js/particles/particlesMain.js
@@ -1,16 +1,6 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'js/particles/cards/card.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-particlesJS.load('particles-js2', 'js/particles/cards/card.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-particlesJS.load('particles-js3', 'js/particles/cards/card.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-particlesJS.load('particles-js4', 'js/particles/cards/card.json', function() {
-  console.log('callback - particles.js config loaded');
+$(document).ready(function() {
+  particlesJS.load('particles-js', 'js/particles/cards/card.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
 });
